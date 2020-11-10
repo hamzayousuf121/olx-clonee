7424e12ed05a985021d2025d2e20f132655f2acb
Hamza Yousuf <hamza.yousuf@treasurycube.com>
2020-11-11 02:23:32 +0500

    Remove Olx Old Folder

--- olx/src/Redux/index.js
+++
@@ -1,8 +0,0 @@
-import reducer from './Reducers/index'
-
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-
-const store = createStore(reducer, applyMiddleware(thunk))
-
-export default store;
